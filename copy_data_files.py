e0f65aa453c53d25506c7539d39a6ef69b962eb7
Caleb Kemere <ckemere@gmail.com>
2015-01-01 17:26:16 -0600

    More robust to errors, added reverse run/day option

--- copy_data_files.py
+++ copy_data_files.py
@@ -6,25 +6,28 @@ import sys
 import argparse
 import re
 import time
+import filecmp
 
 parser = argparse.ArgumentParser(description=
      'Copy NSpike .dat files into individual directories');
-parser.add_argument('-s', '--sourcedir',
+parser.add_argument('-s', '--source-dir',
     help='Source directory containing .dat files');
-parser.add_argument('-d', '--destdir', 
+parser.add_argument('-d', '--dest-dir', 
     help='Destination directory for .dat file directories');
 parser.add_argument('--add-dates', help='Append file date to directory name',
+    action='store_true');
+parser.add_argument('--reverse-order', help='Reverse default order from Day/Run to Run/Day',
     action='store_true');
 
 args = parser.parse_args();
 
-if args.sourcedir :
-  SourceDirectory = args.sourcedir;
+if args.source_dir :
+  SourceDirectory = args.source_dir;
 else :
   SourceDirectory = os.getcwd();
 
-if args.destdir :
-  DestinationDirectory = args.destdir;
+if args.dest_dir :
+  DestinationDirectory = args.dest_dir;
 else :
   DestinationDirectory = os.getcwd();
 
@@ -46,30 +49,49 @@ for f in DatFiles:
       time.gmtime(os.path.getctime(FullFileName)));
   #print "File ", FullFileName, "Just file ", os.path.basename(FullFileName), " Date:", FileDate
 
-  fileregex = re.search(
-      'Rat(?P<name>\w*)_Day(?P<day>\w*)_Run(?P<run>\w*)\.*(?P<extra>.*)\.(?P<machine>\w*)\.dat$', 
-      os.path.basename(FullFileName))
+  if args.reverse_order is not True :
+    fileregex = re.search(
+        'Rat(?P<name>\w*)_Day(?P<day>\w*)_Run(?P<run>\w*)\.*(?P<extra>.*)\.(?P<machine>\w*)\.dat$', 
+        os.path.basename(FullFileName))
+  else :
+    fileregex = re.search(
+        'Rat(?P<name>\w*)_Run(?P<run>\w*)_Day(?P<day>\w*)\.*(?P<extra>.*)\.(?P<machine>\w*)\.dat$', 
+        os.path.basename(FullFileName))
 
   if fileregex is None:
     print "File error: ", FullFileName
     continue;
 
-  if args.add_dates :
-    AnimalDirName = 'Rat%s_Day%003d_Run%s_%s' % \
-                  (fileregex.group('name'), int(fileregex.group('day')),
-                     fileregex.group('run'), FileDate)
-  else:
-    AnimalDirName = 'Rat%s_Day%003d_Run%s' % \
-                  (fileregex.group('name'), int(fileregex.group('day')),
-                      fileregex.group('run'))
-  
-  if os.path.exists(os.path.join(DestinationDirectory,AnimalDirName)) :
-    AnimalDirName = AnimalDirName + '.2'
+  RatName = fileregex.group('name')
+  try:
+    ExperimentDay = '%003d' % int(fileregex.group('day'))
+  except ValueError:
+    ExperimentDay = fileregex.group('day')
+  ExperimentRun = fileregex.group('run');
 
+  if args.add_dates :
+    AnimalDirName = 'Rat%s_Day%s_Run%s_%s' % \
+                  (RatName, ExperimentDay, ExperimentRun, FileDate)
+  else:
+    AnimalDirName = 'Rat%s_Day%s_Run%s' % \
+                  (RatName, ExperimentDay, ExperimentRun)
+  
+
+  destfile = os.path.join(DestinationDirectory, AnimalDirName, AnimalDirName + '.dat')
+  if os.path.exists(destfile) :
+    # compare destination file and source and create a new dir if different
+    if filecmp.cmp(FullFileName,destfile) is not True :
+      print "Creating a new directory for", AnimalDirName
+      AnimalDirName = AnimalDirName + '.2'
+      DestinationFileName = os.path.join(DestinationDirectory, AnimalDirName, AnimalDirName + '.dat')
+    else :
+      print "Ignoring existing", AnimalDirName
+      continue;
+
+  DestinationFileName = os.path.join(DestinationDirectory, AnimalDirName, AnimalDirName + '.dat')
   os.makedirs(os.path.join(DestinationDirectory,AnimalDirName))
 
   print AnimalDirName
 
-  shutil.copy2(FullFileName, os.path.join(DestinationDirectory, AnimalDirName, AnimalDirName +
-    '.dat'));
+  shutil.copy2(FullFileName, DestinationFileName);
 
